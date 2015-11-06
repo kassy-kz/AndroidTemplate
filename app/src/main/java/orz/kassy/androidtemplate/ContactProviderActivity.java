e6a496611df5480a5d2d2a678433364a2ed6b313
kashimoto <kashimoto.kazutoshi@gmail.com>
2015-11-06 22:45:45 +0900

    ContactProvider 追加（サンプルそのまま）

--- app/src/main/java/orz/kassy/androidtemplate/ContactProviderActivity.java
+++ app/src/main/java/orz/kassy/androidtemplate/ContactProviderActivity.java
@@ -11,11 +11,13 @@ import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -24,13 +26,10 @@ import java.util.List;
 import orz.kassy.androidtemplate.data.ContactListItem;
 
 public class ContactProviderActivity extends ListActivity
-            implements LoaderManager.LoaderCallbacks<Cursor>{
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
-
-    ListView mListView;
     // This is the Adapter being used to display the list's data
     SimpleCursorAdapter mAdapter;
-
 
     // These are the Contacts rows that we will retrieve
     static final String[] PROJECTION = new String[] {ContactsContract.Data._ID,
@@ -41,86 +40,60 @@ public class ContactProviderActivity extends ListActivity
             ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
             ContactsContract.Data.DISPLAY_NAME + " != '' ))";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_contact_provider);
-//        mListView = (ListView) findViewById(R.id.list_contact);
-//        mListView.setEmptyView(findViewById(R.id.empty_contact));
 
-//        Cursor cursor = getContentResolver().query(Data.CONTENT_URI,
-//                                                   new String[] {Data._ID, Phone.NUMBER, Phone.TYPE, Phone.LABEL},
-//                                                   Data.CONTACT_ID + "=?" + " AND " + Data.MIMETYPE + "='" + Phone.CONTENT_ITEM_TYPE + "'",
-//                                                   //new String[] {String.valueOf(contactId)},
-//                                                   null,
-//                                                   null);
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setIndeterminate(true);
+        getListView().setEmptyView(progressBar);
 
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+        // For the cursor adapter, specify which columns go into which views
         String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
-        int[] toViews = {android.R.id.text1};
+        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
+
+        // Create an empty adapter we will use to display the loaded data.
+        // We pass null for the cursor, then update it in onLoadFinished()
         mAdapter = new SimpleCursorAdapter(this,
                 android.R.layout.simple_list_item_1, null,
                 fromColumns, toViews, 0);
+        setListAdapter(mAdapter);
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
         getLoaderManager().initLoader(0, null, this);
     }
 
     // Called when a new Loader needs to be created
-    @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
         return new CursorLoader(this, ContactsContract.Data.CONTENT_URI,
                 PROJECTION, SELECTION, null, null);
     }
 
-    // Called when a previously created loader has finished loadin
-    @Override
+    // Called when a previously created loader has finished loading
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
         mAdapter.swapCursor(data);
     }
 
     // Called when a previously created loader is reset, making the data unavailable
-    @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
         mAdapter.swapCursor(null);
     }
 
-    private class ContactListAdapter extends BaseAdapter {
-
-
-        private final List<ContactListItem> mDataList;
-
-        public ContactListAdapter(List<ContactListItem> dataList) {
-            super();
-            mDataList = dataList;
-        }
-
-        @Override
-        public int getCount() {
-            return mDataList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return mDataList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if(convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.view_contact_list_row, null);
-            }
-            ContactListItem item = (ContactListItem) getItem(position);
-            if(item == null) {
-                return null;
-            }
-            ((TextView)convertView.findViewById(R.id.txtContactName)).setText(item.getName());
-            return convertView;
-        }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Do something when a list item is clicked
     }
-
 }
